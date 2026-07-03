8b6510a20ed88913794c18ef3e854bb244f64227
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-07-02 22:21:00 -0300

    Iniciando collections

---
+++ src/SantanderJavaBootCamp/CollectionsEClassesUteis/ListasEArrays/User.java
@@ -0,0 +1,42 @@
+package SantanderJavaBootCamp.CollectionsEClassesUteis.ListasEArrays;
+
+import java.util.Objects;
+
+public class User {
+
+    private int code;
+
+    private String name;
+
+    public User(){
+
+    }
+
+    public User(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        var isEqual = false;
+        if (obj instanceof User user){
+            if (this == user) isEqual = true;
+            if (this.code == user.code && Objects.equals(this.name, user.name)) isEqual = true;
+        }
+        return isEqual;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("{ 'code ': %s, 'name' : %s }", this.code , this.name);
+    }
+}
---
+++ src/SantanderJavaBootCamp/CollectionsEClassesUteis/Set/Main.java
@@ -0,0 +1,19 @@
+package SantanderJavaBootCamp.CollectionsEClassesUteis.Set;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    static void main() {
+        Set<User> users = new TreeSet<>(Comparator.comparingInt(User::getId).reversed());;
+        users.add(new User(1, "Guilherme"));
+        users.add(new User(2, "Pedro"));
+        users.add(new User(3, "Maria"));
+        users.add(new User(4, "João"));
+        users.forEach(System.out::println);
+
+        System.out.println(users);
+    }
+}
