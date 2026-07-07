cb095535a7a64b03eb44d36cf707601f1199ef05
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-07-06 22:03:17 -0300

    LeetCode exercise reverseVowels

---
+++ src/leetcodechallenges/arraystring/Main.java
@@ -0,0 +1,13 @@
+package leetcodechallenges.arraystring;
+
+public class Main {
+    static void main() {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseVowels("IceCreAm"));
+        System.out.println(solution.reverseVowels("hello"));
+        System.out.println(solution.reverseVowels("leetcode"));
+        System.out.println(solution.reverseVowels("aA"));
+        System.out.println(solution.reverseVowels("bcdfg"));
+        System.out.println(solution.reverseVowels("a"));
+    }
+}
---
+++ src/leetcodechallenges/arraystring/Solution.java
@@ -0,0 +1,27 @@
+package leetcodechallenges.arraystring;
+
+import java.util.Stack;
+
+public class Solution {
+    public String reverseVowels(String s) {
+        Stack<Character> vowelsStack = new Stack<>();
+        for (int i = 0; i < s.length(); i++){
+            char c = Character.toLowerCase(s.charAt(i));
+            if (c == 'a' || c == 'e'|| c == 'i' || c == 'o' || c == 'u') {
+               vowelsStack.push(s.charAt(i));
+            }
+        }
+        StringBuilder reversedStringBuilder = new StringBuilder();
+        for (int j = 0; j < s.length(); j++) {
+            char c = Character.toLowerCase(s.charAt(j));
+            if (c == 'a' || c == 'e'|| c == 'i' || c == 'o' || c == 'u'){
+                reversedStringBuilder.append(vowelsStack.pop());
+            } else {
+                reversedStringBuilder.append(s.charAt(j));
+            }
+        }
+
+        return reversedStringBuilder.toString();
+
+    }
+}
