414f957bcfc5eb631619ece2724520d4d588d13c
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-07-30 18:34:27 -0300

    Resolvendo isSubsequence LeetCode

---
+++ src/leetcodechallenges/twopointers/issubsequence/Main.java
@@ -0,0 +1,16 @@
+package leetcodechallenges.twopointers.issubsequence;
+
+public class Main {
+    static void main() {
+        Solution solution = new Solution();
+
+        System.out.println(
+        solution.isSubsequence("abc", "ahbgdc")
+        );
+
+        System.out.println(
+        solution.isSubsequence("axc", "ahbgdc")
+        );
+
+    }
+}
