fc97c1aafae3f9c8c741d16d9f31d0aaa9225f68
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-07-29 21:22:32 -0300

    Resolvendo RecentCounter LeetCode

---
+++ src/leetcodechallenges/queue/numberofrecentcalls/RecentCounter.java
@@ -0,0 +1,31 @@
+package leetcodechallenges.queue.numberofrecentcalls;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RecentCounter {
+
+    Queue<Integer> queue = new LinkedList<>();
+
+    public RecentCounter() {
+
+    }
+
+    public int ping(int t) {
+        queue.add(t);
+
+        while(!queue.isEmpty() && queue.peek() < t - 3000){
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
+
+/*
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
+
