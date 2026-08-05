d2d44f52422d79a5953e3e411688e9db04336d01
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-08-04 22:19:26 -0300

    Resolvendo maxDepth LeetCode

---
+++ src/leetcodechallenges/binarytree_dfs/maximumdepthofbinarytree/Main.java
@@ -0,0 +1,4 @@
+package leetcodechallenges.binarytree_dfs.maximumdepthofbinarytree;
+
+public class Main {
+}
---
+++ src/leetcodechallenges/binarytree_dfs/maximumdepthofbinarytree/Solution.java
@@ -0,0 +1,20 @@
+package leetcodechallenges.binarytree_dfs.maximumdepthofbinarytree;
+
+import javax.swing.tree.TreeNode;
+
+
+public class Solution {
+    /*
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+
+     */
+}
